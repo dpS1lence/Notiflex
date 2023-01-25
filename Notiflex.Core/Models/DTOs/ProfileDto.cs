0fef42eedf9026b725472493083775bfad3ad8e1
davidpetkov <davidpetkov2006@gmail.com>
2023-01-25 19:44:31 +0200

    Added 2 new templates and made some changes to the SendMessage method.

--- Notiflex.Core/Models/DTOs/ProfileDto.cs
+++ Notiflex.Core/Models/DTOs/ProfileDto.cs
@@ -8,6 +8,18 @@ namespace Notiflex.Core.Models.DTOs
 {
     public class ProfileDto
     {
+        public string? HomeTown { get; set; }
+
+        public string? DefaultTime { get; set; }
+
+        public string? ProfilePic { get; set; }
+
+        public string? Description { get; set; }
+
+        public string LastName { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
         public string TelegramChatId { get; set; } = null!;
     }
 }
