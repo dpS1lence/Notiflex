f3fffd24d8262a1715f01b43ae17c3a95609f0b1
davidpetkov <davidpetkov2006@gmail.com>
2023-01-27 19:27:45 +0200

    Changed MessageSender Logic

---
+++ Notiflex.Core/Services/Contracts/IMessageConfigurer.cs
@@ -0,0 +1,20 @@
+﻿using Notiflex.Core.Services.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+
+namespace Notiflex.Core.Services.Contracts
+{
+    public interface IMessageConfigurer
+    {
+        Task<List<string>> ConvertNameToCoordinates(string cityName);
+
+        Task<Message> ConfigureWeatherReportMessage(string name);
+
+        Task<WeatherReportReturnType> ConfigureWeatherReportWithFile(string name);
+    }
+}
--- Notiflex.Core/Services/Contracts/IMessageSender.cs
+++ Notiflex.Core/Services/Contracts/IMessageSender.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using Notiflex.Core.Services.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types;
 
 namespace Notiflex.Core.Services.Contracts
 {
     public interface IMessageSender
     {
-        Task<string> SendMessage(string message, string chatId);
-        Task<string> ConfigureWeatherReport(string name);
-        Task<List<string>> ConvertNameToCoordinates(string cityName);
+        Task SendMessageWithFile(WeatherReportReturnType report, string chatId);
+
+        Task SendMessage(Message message, string chatId);
     }
 }
