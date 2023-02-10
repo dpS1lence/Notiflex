a8508e42151a9d69c6cf0fc36edd4a4f90504f0f
ImSk1 <DavidH@students.softuni.bg>
2023-02-10 20:17:44 +0200

    azure deploy

---
+++ Notiflex/Areas/Home/Controllers/ErrorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notiflex.Core.Exceptions;
+using Notiflex.ViewModels;
+using System.Diagnostics;
+
+namespace Notiflex.Areas.Home.Controllers
+{
+    [AllowAnonymous]
+    [Area("Home")]
+    public class ErrorController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            IExceptionHandlerPathFeature? exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            Exception error = exceptionHandlerPathFeature?.Error!;
+            switch (error)
+            {
+                case NotFoundException:
+                    return RedirectToAction("ErrorPage", new { statusCode = 404 });
+                case ArgumentException:
+                    return RedirectToAction("ErrorPage", new { statusCode = 500 });
+
+                default: return RedirectToAction("ErrorPage", new { statusCode = 500 });
+            }
+        }
+        public IActionResult ErrorPage(int statusCode)
+        {
+            ViewData["statusCode"] = statusCode;
+            return View();
+        }
+    }
+}
