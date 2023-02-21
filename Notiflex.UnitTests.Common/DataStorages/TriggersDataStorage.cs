415199c38bcea290deb461c0900e505079a385e7
davidpetkov <davidpetkov2006@gmail.com>
2023-02-21 17:10:55 +0200

    Created Data Storage

---
+++ Notiflex.UnitTests.Common/DataStorages/TriggersDataStorage.cs
@@ -0,0 +1,68 @@
+﻿using Notiflex.Infrastructure.Data.Models.ScheduleModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notiflex.UnitTests.Common.DataStorages
+{
+    public static class TriggersDataStorage
+    {
+        private static readonly NotiflexTrigger trigger = new()
+        {
+            Id = 1,
+            City = "Sofia",
+            Interval = 1000,
+            UserId = "1",
+            Hour = 2,
+            Minutes = "20",
+            Name = "TestTrigger",
+            DaysOfWeek = "Monday, Friday"
+        };
+
+        private static readonly NotiflexTrigger triggerWrongCity = new()
+        {
+            Id = 1,
+            City = "asdasdasdasdasd",
+            Interval = 1000,
+            UserId = "1",
+            Hour = 2,
+            Minutes = "20",
+            Name = "TestTrigger",
+            DaysOfWeek = "Monday, Friday"
+        };
+
+        private static readonly NotiflexTrigger triggerWrongUserId = new()
+        {
+            Id = 1,
+            City = "Sofia",
+            Interval = 1000,
+            UserId = "-1",
+            Hour = 2,
+            Minutes = "20",
+            Name = "TestTrigger",
+            DaysOfWeek = "Monday, Friday"
+        };
+
+        private static readonly NotiflexTrigger triggerWrongTime = new()
+        {
+            Id = 1,
+            City = "Sofia",
+            Interval = 1000,
+            UserId = "1",
+            Hour = 222,
+            Minutes = "222",
+            Name = "TestTrigger",
+            DaysOfWeek = "Monday, Friday"
+        };
+
+        public static NotiflexTrigger Trigger { get => trigger; }
+
+        public static NotiflexTrigger TriggerWrongCity { get => triggerWrongCity; }
+
+        public static NotiflexTrigger TriggerWrongUserId { get => triggerWrongUserId; }
+
+        public static NotiflexTrigger TriggerWrongTime { get => triggerWrongTime; }
+    }
+}
---
+++ Notiflex.UnitTests.Common/DataStorages/UsersDataStorage.cs
@@ -0,0 +1,107 @@
+﻿using Notiflex.Infrastructure.Data.Models.UserModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notiflex.UnitTests.Common.DataStorages
+{
+    public class UsersDataStorage
+    {
+        private static readonly NotiflexUser notiflexUserDefault = new()
+        {
+            Id = "1",
+            FirstName = "Pesho",
+            LastName = "Peshov",
+            Age = "02-12-2012",
+            Description = "Hi i am Pesho!",
+            Gender = "Male",
+            ProfilePic = "image.url",
+            TelegramInfo = "1234",
+            DefaultTime = "12:20",
+            HomeTown = "Sofia",
+            UserName = "peshkata",
+            Email = "pesho@abc.cba",
+            EmailConfirmed = true
+        };
+
+        private static readonly NotiflexUser notiflexUserNoTelegramInfo = new()
+        {
+            Id = "1",
+            FirstName = "Pesho",
+            LastName = "Peshov",
+            Age = "02-12-2012",
+            Description = "Hi i am Pesho!",
+            Gender = "Male",
+            ProfilePic = "image.url",
+            TelegramInfo = null,
+            DefaultTime = "12:20",
+            HomeTown = "Sofia",
+            UserName = "peshkata",
+            Email = "pesho@abc.cba",
+            EmailConfirmed = true
+        };
+
+        private static readonly NotiflexUser notiflexUserNoHomeTown = new()
+        {
+            Id = "1",
+            FirstName = "Pesho",
+            LastName = "Peshov",
+            Age = "02-12-2012",
+            Description = "Hi i am Pesho!",
+            Gender = "Male",
+            ProfilePic = "image.url",
+            TelegramInfo = "1234",
+            DefaultTime = "12:20",
+            HomeTown = null,
+            UserName = "peshkata",
+            Email = "pesho@abc.cba",
+            EmailConfirmed = true
+        };
+
+        private static readonly NotiflexUser notiflexUserEmailNotConfirmed = new()
+        {
+            Id = "1",
+            FirstName = "Pesho",
+            LastName = "Peshov",
+            Age = "02-12-2012",
+            Description = "Hi i am Pesho!",
+            Gender = "Male",
+            ProfilePic = "image.url",
+            TelegramInfo = "1234",
+            DefaultTime = "12:20",
+            HomeTown = "Sofia",
+            UserName = "peshkata",
+            Email = "pesho@abc.cba",
+            EmailConfirmed = false
+        };
+
+        private static readonly NotiflexUser notiflexUserNoEmail = new()
+        {
+            Id = "1",
+            FirstName = "Pesho",
+            LastName = "Peshov",
+            Age = "02-12-2012",
+            Description = "Hi i am Pesho!",
+            Gender = "Male",
+            ProfilePic = "image.url",
+            TelegramInfo = "1234",
+            DefaultTime = "12:20",
+            HomeTown = "Sofia",
+            UserName = "peshkata",
+            Email = null,
+            EmailConfirmed = true
+        };
+
+        public static NotiflexUser NotiflexUserDefault { get => notiflexUserDefault; }
+
+        public static NotiflexUser NotiflexUserNoTelegramInfo { get => notiflexUserNoTelegramInfo; }
+
+        public static NotiflexUser NotiflexUserNoHomeTown { get => notiflexUserNoHomeTown; }
+
+        public static NotiflexUser NotiflexUserEmailNotConfirmed { get => notiflexUserEmailNotConfirmed; }
+
+        public static NotiflexUser NotiflexUserNoEmail { get => notiflexUserNoEmail; }
+    }
+}
